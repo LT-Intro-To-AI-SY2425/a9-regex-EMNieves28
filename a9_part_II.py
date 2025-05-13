72a891ee6b87d82a82c5102b8de2c49bbaafbb18
Eliana Nieves <EMNieves@cps.edu>
2025-05-13 11:18:31 -0500

    finished part 2

--- a9_part_II.py
+++ a9_part_II.py
@@ -38,8 +38,8 @@ def get_birth_date(name: str) -> str:
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
     # TODO: fill this in
-    # print(infobox_text)
-    pattern = "(Born)(?P<name>\D+)(?P<birth>\d{4}-\d{2}-\d{2})"
+    print(infobox_text)
+    pattern = "Born\D*(?P<birth>\d{4}-\d{2}-\d{2})"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     assert get_birth_date("Anita Borg") == "1949-01-17", "Incorrect birth date for Anita Borg"
     print('\n<<<< Birth date tests passed >>>>')
 
-    # print('\n<<<< All tests passed! >>>>')
+    print('\n<<<< All tests passed! >>>>')
